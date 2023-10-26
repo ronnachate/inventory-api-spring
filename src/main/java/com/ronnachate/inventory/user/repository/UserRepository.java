12fd3c3a926b1450b91c54b2c2c9ef27c90b9526
Ronnachate <ronnachate@gmail.com>
2023-10-26 15:15:10 +0700

    Add user service.

--- src/main/java/com/ronnachate/inventory/user/repository/UserRepository.java
+++ src/main/java/com/ronnachate/inventory/user/repository/UserRepository.java
@@ -1,9 +1,10 @@
 package com.ronnachate.inventory.user.repository;
 
 import java.util.UUID;
-import org.springframework.data.repository.PagingAndSortingRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ronnachate.inventory.user.entity.User;
 
-public interface UserRepository extends PagingAndSortingRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
---
+++ src/main/java/com/ronnachate/inventory/user/service/UserService.java
@@ -0,0 +1,20 @@
+package com.ronnachate.inventory.user.service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ronnachate.inventory.user.entity.User;
+import com.ronnachate.inventory.user.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> getById(UUID id) {
+        return userRepository.findById(null);
+    }
+}
