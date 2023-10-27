b681a028481381cf6a7c8219ac93ce8b88fadbf9
Ronnachate <ronnachate@gmail.com>
2023-10-27 12:42:15 +0700

    Add service.getUser with unit test.

--- src/main/java/com/ronnachate/inventory/user/service/UserService.java
+++ src/main/java/com/ronnachate/inventory/user/service/UserService.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.ronnachate.inventory.user.entity.User;
@@ -17,5 +19,11 @@ public class UserService {
     public Optional<User> getById(UUID id) {
         var user = userRepository.findById(id);
         return user;
+    }
+
+    public Page<User> getUsers(int page, int rows) {
+        PageRequest pageRequest = PageRequest.of(page, rows);
+        Page<User> pagingUser = userRepository.findAll(pageRequest);
+        return pagingUser;
     }
 }
--- src/test/java/com/ronnachate/inventory/user/UserServiceTest.java
+++ src/test/java/com/ronnachate/inventory/user/UserServiceTest.java
@@ -6,6 +6,9 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,6 +18,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 
 import com.ronnachate.inventory.user.entity.User;
 import com.ronnachate.inventory.user.repository.UserRepository;
@@ -55,4 +61,51 @@ public class UserServiceTest {
             userService.getById(UUID.randomUUID());
         });
     }
+
+    @Test
+    @DisplayName("Get users shoud return correct page data")
+    void getUsers_should_return_correct_paget_data() {
+        int page = 1;
+        int rows = 10;
+        var mockUser = new User("tile1", "name1", "lastname1", "user1");
+        List<User> users = Arrays.asList(mockUser);
+        Page<User> pagedResponse = new PageImpl<User>(users);
+        PageRequest pageRequest = PageRequest.of(page, rows);
+        when(userRepository.findAll(pageRequest)).thenReturn(pagedResponse);
+        var userPage = userService.getUsers(page, rows);
+        
+        assertEquals(1, userPage.getTotalElements());
+        assertEquals(1, userPage.getTotalPages());
+        var user = userPage.getContent().get(0);
+        assertEquals("name1", user.getName());
+        assertEquals("user1", user.getUsername());
+    }
+
+    @Test
+    @DisplayName("Get users shoud return empty page when no data found")
+    void getUsers_should_return_empty_page_when_no_data_found() {
+        int page = 1;
+        int rows = 10;
+        List<User> users = new ArrayList<User>();
+        Page<User> pagedResponse = new PageImpl<User>(users);
+        PageRequest pageRequest = PageRequest.of(page, rows);
+        when(userRepository.findAll(pageRequest)).thenReturn(pagedResponse);
+        var userPage = userService.getUsers(page, rows);
+        
+        assertEquals(0, userPage.getTotalElements());
+        assertEquals(1, userPage.getTotalPages());
+    }
+
+    @Test
+    @DisplayName("Get users shoud throw error when error found")
+    void getUsers_should_throw_error_when_error_found() {
+        int page = 1;
+        int rows = 10;
+        PageRequest pageRequest = PageRequest.of(page, rows);
+        when(userRepository.findAll(pageRequest)).thenThrow(new ArrayIndexOutOfBoundsException());
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+            userService.getUsers(page, rows);
+        });
+    }
+
 }
