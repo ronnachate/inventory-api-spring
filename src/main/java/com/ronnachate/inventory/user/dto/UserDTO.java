7fd22289deb2cfeccb329a23934edf850345921d
Ronnachate <ronnachate@gmail.com>
2023-10-30 13:17:01 +0700

    Add spring validaion to userDTO.

--- src/main/java/com/ronnachate/inventory/user/dto/UserDTO.java
+++ src/main/java/com/ronnachate/inventory/user/dto/UserDTO.java
@@ -4,16 +4,33 @@ import java.util.Date;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 
 @Getter
 @Setter
 public class UserDTO {
     private UUID id;
+
+    @Size(max = 20, message = "Username have at most 50 characters")
     private String title;
+
+    @NotEmpty(message = "Name is required")
+    @Size(min = 2, message = "Name should have at least 2 characters")
+    @Size(max = 20, message = "Username have at most 100 characters")
     private String name;
+
+    @Size(max = 20, message = "Username have at most 100 characters")
     private String lastname;
+
+    @NotEmpty(message = "Username is required")
+    @Size(min = 2, message = "Username have at least 2 characters")
+    @Size(max = 20, message = "Username have at most 50 characters")
     private String username;
+
     private UserStatusDTO status;
+
     private Date createdAt;
+
     private Date updatedAt;
 }
--- src/main/java/com/ronnachate/inventory/user/dto/UserStatusDTO.java
+++ src/main/java/com/ronnachate/inventory/user/dto/UserStatusDTO.java
@@ -5,5 +5,6 @@ import lombok.Data;
 @Data
 public class UserStatusDTO {
     private long id;
+
     private String name;
 }
